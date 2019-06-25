73df0319272c98ddc6e2c16f8c7a2e2c88c2626a
daniela al <daniela@gmail.com>
2019-06-25 14:59:31 +0300

    Added missing logic: lose when exiting the screen and not allowing to go directly back

--- achtungquick/gamelogic.js
+++ achtungquick/gamelogic.js
@@ -6,10 +6,10 @@ var velx = 0;
 var vely = 50;
 var velx2 = 50
 var vely2 = 0
-var xpos = 250;
-var ypos = 450;
-var xpos2 = 300;
-var ypos2 = 300;
+var xpos =  getRandomCord();
+var ypos =  getRandomCord();
+var xpos2 = getRandomCord();
+var ypos2 = getRandomCord();
 var currcolor1;
 var currcolor2;
 var colorcount = 0;
@@ -18,42 +18,44 @@ var intr = setInterval(update, 100);
 function checkKey(e) {
 
     e = e || window.event;
-    if (e.keyCode == '38') {
-        vely = -50
-        velx = 0
-        // up arrow
+    if (e.keyCode == '38' && vely != 50) {
+			vely = -50
+			velx = 0
+			// up arrow
     }
-    else if (e.keyCode == '40') {
-        vely = 50
-        velx = 0
-        // down arrow
+    else if (e.keyCode == '40' && vely != -50) {
+			vely = 50
+			velx = 0
+			// down arrow
     }
-    else if (e.keyCode == '37') {
-        vely = 0
-        velx = -50
-       // left arrow
+    else if (e.keyCode == '37' && velx != 50 ) {
+			vely = 0
+			velx = -50
+		   // left arrow
     }
-    else if (e.keyCode == '39') {
-        vely = 0
-        velx = 50
-       // right arrow
-    } else if (e.keyCode == '87') {
-        // w
-        vely2 = -50
-        velx2= 0
-
-    } else if (e.keyCode == '68') {
-        vely2 = 0
-        velx2 = 50
-        // d
-    } else if (e.keyCode == '83') {
-        vely2 = 50
-        velx2 = 0
-        //s 
-    } else if (e.keyCode == '65' ) {
-        /* a */
-        vely2 = 0
-        velx2 = -50
+    else if (e.keyCode == '39' && velx != -50) {
+			vely = 0
+			velx = 50
+		   // right arrow
+	}
+    else if (e.keyCode == '87' && vely2 != 50 ) {
+			// w
+			vely2 = -50
+			velx2= 0
+    } else if (e.keyCode == '68' && velx2 != -50 ) {
+		
+			vely2 = 0
+			velx2 = 50
+			// d
+    } else if (e.keyCode == '83' && vely2 != -50) {
+		
+			vely2 = 50
+			velx2 = 0
+			//s 
+    } else if (e.keyCode == '65' && velx2 != 50) {
+			/* a */
+			vely2 = 0
+			velx2 = -50
     }
 
 
@@ -76,21 +78,19 @@ function update() {
     var hex1 = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
     p = ctx.getImageData(xpos2 + velx2, ypos2 + vely2, 1, 1).data;
     var hex2 = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-    if ((hex1 != "#000000" && hex1 != "#ffffff") || (hex2 != "#000000" && hex2 != "#ffffff"))  {
-        clearInterval(intr)
-        console.log(hex1 + hex2)
-        
-        if (hex1 == "#ffffff" || hex1 != "#000000") {
-            ctx.fillStyle = currcolor1;
-            ctx.font = "200px Arial";
-            ctx.fillText("Player1 loses", 2000 % xpos, 2000 % ypos);
-        } else {
-            ctx.fillStyle = currcolor2;
-            ctx.font = "200px Arial";
-            ctx.fillText("Player2 loses",2000 % xpos2, 2000 % ypos2);
-        }
-        
-    }
+	
+	if(playerLost(hex1, xpos, ypos)){
+		ctx.fillStyle = currcolor1;
+        ctx.font = "200px Arial";
+		ctx.fillText("Player1 loses", calculateXMessage(xpos), calculateYMessage(ypos));
+	}
+	
+	if(playerLost(hex2, xpos2, ypos2)){
+		ctx.fillStyle = currcolor2;
+        ctx.font = "200px Arial";
+        ctx.fillText("Player2 loses",calculateXMessage(xpos2), calculateYMessage(ypos2));
+	}
+	
     xpos += velx;
     ypos += vely;
     xpos2 += velx2;
@@ -102,4 +102,32 @@ function update() {
     ctx.fillStyle = currcolor2;
     ctx.fillRect(xpos, ypos, 50, 50);
     console.log(hex1 + hex2);
+}
+
+function playerLost(hex, xpos, ypos){
+		
+		if ((hex != "#000000" && hex != "#ffffff") || xpos <= 0 || ypos <= 0 || xpos >= 3000 || ypos >= 3000){
+			clearInterval(intr);
+			return true;
+		}
+		return false;
+		
+}
+
+function calculateYMessage(ypos){
+	if(ypos < 200)
+		return 200;
+	if (ypos > 2900)
+		return 2900;
+	return ypos;
+}
+
+function calculateXMessage(xpos){
+	if(xpos > 2800)
+		return 2800;
+	return xpos;
+}
+
+function getRandomCord(){
+	return Math.floor(Math.random() * 1800) + 600;
 }
